860112c903b0b0bd789c5e71ef9bfbf785e38a41
nikeshthapa255 <nikeshthapa255@gmail.com>
2019-07-21 20:02:44 +0530

    Updates

---
+++ precache-manifest.380b446bc17995e0e137bfa67f6303fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9370cdbd0d9f2eb9402e79fee74cea00",
+    "url": "/RestroPlaza/index.html"
+  },
+  {
+    "revision": "2ae188a374961bb6e85a",
+    "url": "/RestroPlaza/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/RestroPlaza/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "2ae188a374961bb6e85a",
+    "url": "/RestroPlaza/static/js/main.3e58c289.chunk.js"
+  },
+  {
+    "revision": "86345318a9ffabb44da2",
+    "url": "/RestroPlaza/static/js/runtime~main.a999eec3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/RestroPlaza/static/media/logo.5d5d9eef.svg"
+  }
+]);
